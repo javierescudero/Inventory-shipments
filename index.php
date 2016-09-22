9e0d4fd24e2952c712ccc031269d09a5e6990aa6
javierescudero <j.escudero.g@hotmail.com>
2016-09-21 23:02:00 -0600

    arreglos

--- index.php
+++ index.php
@@ -6,6 +6,27 @@
 	<title>Inventory Shipments</title>
 </head>
 <body>
-	
+	<?php
+	if (isset($_POST["iniciar"])) {
+		$number_part = array($_POST["part_number"]);
+		$quantity = array($_POST["quantity"]);
+		$wo = array($_POST["wo"]);
+
+		$lenght = sizeof($number_part);
+
+		/*for($i=0;$i<=$lenght;$i++){
+		    echo $array[$i]
+		}*/
+
+		echo "".$number_part[0];
+	}
+	?>
+	<form action="" method="post">
+		<input type="number" id="" name="" value="Nuevo Pallet" min="0"><br><br>
+		<label for="part-number">Part Number</label><input type="text" id="part_number" name="part_number"><br>
+		<label for="quantity">Quantity</label><input type="text" id="quantity" name="quantity"><br>
+		<label for="wo">W.O.</label><input type="text" id="wo" name="wo"><br>
+		<input type="submit" id="" name="iniciar" value="Guardar">
+	</form>
 </body>
 </html>
