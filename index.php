ef50c9ab8a91f7b1fae2bfa17941e66596b02b38
Javier Escudero <j.escudero.g@hotmail.com>
2016-09-23 16:22:01 -0600

    Imprime datos pero los repite

--- index.php
+++ index.php
@@ -20,34 +20,30 @@
 			var c = $('#codes').val();
 			
 			if (c.charAt(0) == 'P') {
-				//array_pN[0] = c.substring(1);
 				var pN = c.substring(1);
 				array_pN.push(pN);
-				/*for (var i = 0; i < array_pN.length; i++) {
-					alert("Part Number --> " + array_pN[i]);
-				}*/
-				//alert("Part Number --> " + array_pN[0]);
 				$('#codes').val('');
 
 			} else if (c.charAt(0) == 'Q') {
-				//array_Q[0] = c.substring(1);
 				var Q = c.substring(1);
 				array_Q.push(Q);
-				//alert("Quantity --> " + array_Q[0]);
 				$('#codes').val('');
 
 			} else if (c.charAt(0) == 'W') {
-				//array_WO[0] = c.substring(1);
 				var WO = c.substring(1);
 				array_WO.push(WO);
-				if (array_WO.length < 1) {
-					alert("Boxes: " + parseInt(boxes));
-				} else {
-					alert("Boxes: " + parseInt(boxes+1));
-				}
-				//alert("WO --> " + array_WO[0]);
-				$('#codes').val('');
+				//$("#table tr").remove();
+				if (array_WO.length >= 1) {
 
+					alert("Boxes: " + parseInt(boxes));
+					for (var i = 0; i < array_pN.length; i++) {
+						$("#part_number").append('<tr><td>'+array_pN[i]+'</td></tr>');
+						$("#quantity").append('<tr><td>'+array_Q[i]+'</td></tr>');
+						$("#wo").append('<tr><td>'+array_WO[i]+'</td></tr>');
+					};
+					boxes++;
+				}
+				$('#codes').val('');
 			}
 			return false;
 		});
@@ -55,7 +51,23 @@
 </script>
 <body>
 	<form id="form">
-		<label for="codes">Codes</label><input type="text" id="codes" name="codes" style="width:15%"><br><br>
+		<label for="codes">Codes</label><input type="text" id="codes" name="codes" style="width:10%"><br><br>
+		<table id="table" cellpadding="3" cellspacing="3" border="1">
+			<thead>
+				<tr>
+					<th>Part Number</th>
+					<th>Quantity</th>
+					<th>WO</th>
+				</tr>
+			</thead>
+			<tbody id="body">
+				<tr>
+					<td id="part_number"></td>
+					<td id="quantity"></td>
+					<td id="wo"></td>
+				</tr>
+			</tbody>
+		</table>
 	</form>
 </body>
 </html>
