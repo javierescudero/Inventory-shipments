81a4ee46c8efd0fe9159d77bd300fbf094cc9cb0
Javier Escudero <j.escudero.g@hotmail.com>
2016-09-28 16:55:18 -0600

    Pendiente agregar codigo de pallet

--- indexOr.php
+++ indexOr.php
@@ -4,31 +4,37 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<title>Inventory Shipments</title>
-	<script src="jquery-1.12.4.min.js"></script>
-	<script language="javascript">
-		$(document).ready(function() {
-		     $("#saveData").click(function(event) {
-			     $("#datos_a_enviar").val( $("<div>").append( $("#table").eq(0).clone()).html());
-			     $("#form").submit();
-			});
-		});
-	</script>
+	<script type="text/javascript" src="js/jquery-1.12.4.min.js"></script>
+	<script type="text/javascript" src="js/tableExport.js"></script>
+	<script type="text/javascript" src="js/jquery.base64.js"></script>
+	<script type="text/javascript" src="js/html2canvas.js"></script>
+	<script type="text/javascript" src="js/jspdf/libs/sprintf.js"></script>
+	<script type="text/javascript" src="js/jspdf/jspdf.js"></script>
+	<script type="text/javascript" src="js/jspdf/libs/base64.js"></script>
 </head>
 <script type="text/javascript">
 	$(document).ready(function(){
 
 		$('#codes').focus();
 
+		var array_P = new Array();
 		var array_pN = new Array();
 		var array_Q = new Array();
 		var array_WO = new Array();
 
-		var boxes = 1;
+		var boxes = 1, pallets = 1;
 
 		$('#form').submit(function() {
 			var c = $('#codes').val();
 
-				if (c.charAt(0) == 'P') {
+				if (c.charAt(0) == 'T') {
+					var P = c.substring(1);
+					array_P.push(P);
+					$('#codes').val('');
+					alert("Pallet: " + parseInt(pallets));
+					pallets++;
+
+				} else if (c.charAt(0) == 'P') {
 					var pN = c.substring(1);
 					array_pN.push(pN);
 					$('#codes').val('');
@@ -49,47 +55,46 @@
 					$('#codes').val('');
 					for (var i = 0; i < array_pN.length; i++) {
 						//$("#body").remove();
-						$("#part_number").append('<tr><td>'+array_pN[i]+'</td><td>');
+						$("#body").append('<tr><td>'+array_P[i]+'</td>'+'<td>'+array_pN[i]+'</td>'+'<td>'+array_Q[i]+'</td>'+'<td>'+array_WO[i]+'</td></tr>');
+						/*$("#part_number").append('<tr><td>'+array_pN[i]+'</td></tr>');
 						$("#quantity").append('<tr><td>'+array_Q[i]+'</td></tr>');
-						$("#wo").append('<tr><td>'+array_WO[i]+'</td></tr>');
+						$("#wo").append('<tr><td>'+array_WO[i]+'</td></tr>');*/
 					};
 				}
 
 			return false;
 		});
 
-		$("#saveData").click(function (e) {
-		    window.open('data:application/vnd.ms-excel,' + $('#divData').html());
-		    e.preventDefault();
+		$('#saveData').click(function(){
+			$('#data').tableExport({type:'excel',escape:'false'});
 		});
 
 	});
 </script>
 <body>
-	<form id="form" method="post" action="export_excel.php">
+	<form id="form" >
 		<label for="codes">Codes</label><input type="text" id="codes" name="codes" style="width:10%"><br><br>
 		<input type="button" value="Save and Export" id="saveData"><br><br>
-		<input type="hidden" id="datos_a_enviar" name="datos_a_enviar" />
-		
+	</form>
 		<div id="divData">
-			<table id="table" cellpadding="3" cellspacing="3" border="1">
+			<table id="data" cellpadding="1" cellspacing="1" border="1">
 				<thead>
 					<tr>
+						<th>Pallet</th>
 						<th>Part Number</th>
 						<th>Quantity</th>
 						<th>WO</th>
 					</tr>
 				</thead>
 				<tbody id="body">
-					<tr>
+					<!--<tr>
 						<td id="part_number"></td>
 						<td id="quantity"></td>
 						<td id="wo"></td>
-					</tr>
+					</tr>-->
 				</tbody>
 			</table>
 		</div>
-
-	</form>
+</table>
 </body>
 </html>
